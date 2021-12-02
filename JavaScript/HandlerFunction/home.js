8d0f5f53589fd653e06a7c6489aab6d8a08f3010
BalaDipu <dipubala466@gmail.com>
2021-12-02 19:17:41 +0600

    Showing book into home page.

--- JavaScript/HandlerFunction/home.js
+++ JavaScript/HandlerFunction/home.js
@@ -1,63 +1,35 @@
-class Products {
-    async getProducts() {
-        try {
-            let result =await JSON.parse(localStorage.getItem('allBooks'));
+let allBooks = JSON.parse(localStorage.getItem('allBooks'));
+// allBooks.innerHTML = ''
+// let products = result.map(item => {
+//     const title = item.bookTitle;
+//     const author = item.bookAuthor;
+//     const image = item.bookCoverImage;
+//     const price = item.bookPrice;
+//     const isbn = item.bookISBN;
+// });
 
-            let products = result.map(item => {
-                const  title = item.bookTitle;
-                const author = item.bookAuthor;
-                const image = item.bookCoverImage;
-                const price = item.bookPrice;
-                const isbn = item.bookISBN;
-                return { title,author, price, isbn, image };
-            });
-            return products;
-        } catch (error) {
-            console.log(error);
-        }
-    }
+function displayProducts(){
+    const showBooks = document.getElementById('books-center');
+    let result = '';
+    allBooks.map(book => {
+        result += `
+             <!--single book-->
+            <article class="book">
+            <h3>${book.bookTitle}</h3>
+                <div class="img-container">
+                    <img src=${book.bookCoverImage} alt="book" class="book-img">
+                    <button class="bag-btn" data-id=${book.bookISBN}>
+                        <i class="fas fa-shopping-cart"> </i>
+                        add to cart
+                    </button>
+                </div>
+                <h3>${book.bookAuthor}</h3>
+                <h4>tk.${book.bookPrice}</h4>
+            </article>
+            <!--single book end-->     
+       `;
+       showBooks.innerHTML = result
+    });
 }
-
-class UI {
-    displayProducts(products) {
-        let result = '';
-        products.forEach(product => {
-            result += `
-         <!--single product-->
-        <article class="product">
-            <div class="img-container">
-                <img src=${product.image} alt="product" class="product-img">
-                <button class="bag-btn" data-id=${product.id}>
-                    <i class="fas fa-shopping-cart"> </i>
-                    add to cart
-                </button>
-            </div>
-            <h3>${product.title}</h3>
-            <h4>tk.${product.price}</h4>
-        </article>
-        <!--single product end-->     
-   `;
-        });
-        productsDom.innerHTML = result;
-    }
-}
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const ui = new UI();
-    const products = new Products();
-    //setup app
-    ui.setupApp();
-
-    //get all products
-    products
-        .getProducts()
-        .then(products => {
-            ui.displayProducts(products)
-            Storage.saveProducts(products);
-        }).then(() => {
-            ui.getBagButtons();
-            ui.cartLogic();
-        });
-
-});
+  
+window.onload = displayProducts();
